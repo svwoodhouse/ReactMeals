f5179e6bfc0bbdefc169de012fee9c2a221668b4
Sydnee <svwoodhouse@gmail.com>
2022-12-22 22:37:23 -0500

    Added logic to Cart component

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import classes from './Modal.module.css'
-import { ReactDOM } from 'react'
+import ReactDOM from 'react-dom'
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} />
+    return <div className={classes.backdrop} onClick={props.onClose} />
 } 
 
 const ModalOverlay = (props) => {
-    return <div className={classes.modal}>
+    return (<div className={classes.modal}>
         <div className={classes.content}>
             {props.children}
         </div>
-    </div>
+    </div>)
 }
 const portalElement = document.getElementById('overlays')
 
 const Modal = (props) => {
     return (
         <React.Fragment>
-            {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </React.Fragment>
     )
