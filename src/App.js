2bac4b6981bdf1716f5f6dc22b00523e38c0118a
Sydnee <svwoodhouse@gmail.com>
2022-12-27 10:50:36 -0500

    Adding and Removing to Cart logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from './components/Cart/Cart'
+import CartProvider from "./store/CartProvider";
+
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false)
 
@@ -15,11 +17,13 @@ function App() {
 
   return (
     <React.Fragment>
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler}/>
-      <main>
-        <Meals/>
-      </main>
+      <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler}/>}
+        <Header onShowCart={showCartHandler}/>
+        <main>
+          <Meals/>
+        </main>
+      </CartProvider>
     </React.Fragment>
   );
 }
